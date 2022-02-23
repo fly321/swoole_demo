eb38cd90ab50b1197447b07d985a5089377e0cbe
aimer <15267840622@163.com>
2022-02-24 00:04:48 +0800

    websocket

---
+++ server/WebsocketServer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * WebsocketServer.php
+ * 文件描述
+ * created on 23:46 2022/2/23 23:46
+ * create by xiflys
+ */
+
+class WebsocketServer
+{
+    public function onOpen(Swoole\WebSocket\Server $ws,Swoole\Http\Request $request){
+        print_r($request->fd."hello 连接成功");
+    }
+
+    public function onMessage(Swoole\WebSocket\Server $server, Swoole\WebSocket\Frame $frame){
+        print_r(['fd'=>$frame->fd,'data'=>$frame->data,'code'=>$frame->opcode]);
+    }
+
+
+    public function onClose(Swoole\WebSocket\Server $ws,$fd){
+        print_r('client close:'.$fd);
+    }
+}
+
+$obj = new WebsocketServer();
+$ws = new Swoole\WebSocket\Server("0.0.0.0",9501);
+$ws->on('open',array($obj,'onOpen'));
+$ws->on('message',array($obj,'onMessage'));
+$ws->on('close',array($obj,'onClose'));
+$ws->start();
