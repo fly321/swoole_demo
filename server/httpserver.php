eb402ccf14336b0d2649c9ed15866539261adcb6
aimer <15267840622@163.com>
2022-02-23 23:34:32 +0800

    httpserver 访问静态资源 /index.html 就访问index.html

--- server/httpserver.php
+++ server/httpserver.php
@@ -6,7 +6,12 @@
  * create by xiflys
  */
 $server = new \Swoole\Http\Server("0.0.0.0",9501);
+$server->set([
+    'enable_static_handler'=>true,
+    'document_root'=>'/www/wwwroot/swoole_demo/data'
+]);
 $server->on("request", function($request,$response){
+    $response->cookie("textsss","flyber",time()+60);
     $response->end("hahah");
 });
 $server->start();
