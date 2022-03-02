21c17b2e139ff56ff46dd1e351fa7ef46db4120a
aimer <15267840622@163.com>
2022-03-02 22:14:02 +0800

    线程id

---
+++ process/demo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * demo.php
+ * 文件描述
+ * created on 22:04 2022/3/2 22:04
+ * create by xiflys
+ */
+$process = new \Swoole\Process(function(\Swoole\Process $pro){
+    $pro->write("xixiahjh");
+},true);
+# 线程id
+$pid = $process->start();
+echo $pid.PHP_EOL;
