70db77ac33f4ba43ef6275c1effcd7ae37c725f0
aimer <15267840622@163.com>
2022-03-03 23:43:11 +0800

    共享内存

---
+++ memory/table.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * table.php
+ * 文件描述
+ * created on 23:33 2022/3/3 23:33
+ * create by xiflys
+ */
+// 创建内存表
+use Swoole\Table as TableAlias;
+
+$table = new TableAlias(1024);
+
+
+# 内存表增加列
+
+$table->column('id',TableAlias::TYPE_INT,4);
+$table->column('money',TableAlias::TYPE_FLOAT,4);
+$table->column('name',TableAlias::TYPE_STRING,64);
+
+$table->create();
+
+$table->set("hello_fly",array('id'=>1,'money'=>10.1,'name'=>'fly'));
+
+$arr = $table->get('hello_fly');
+var_dump($arr);
+
+
+
